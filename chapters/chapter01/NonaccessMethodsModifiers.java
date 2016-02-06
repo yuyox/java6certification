b20bd3fae67b97a01d12bf161a52edc71ee7c8a1
yuyox <ezequielrmy@yahoo.com.ar>
2016-02-05 20:07:09 -0600

    Yuyox - Chapter 01 - Nonaccess Methods Modifiers

---
+++ chapters/chapter01/NonaccessMethodsModifiers.java
@@ -0,0 +1,9 @@
+package chapters.chapter01;
+
+public class NonaccessMethodsModifiers{
+  public static void main(String [] args){}
+  abstract final void method1(){}
+  synchronized void method2(){}
+  native void method3(){}
+  strictfp void method4();
+}
