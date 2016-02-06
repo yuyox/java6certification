19d2290d5effa8bc175fa2a99976e26e0fa9017e
yuyox <ezequielrmy@yahoo.com.ar>
2016-02-05 19:21:31 -0600

    Yuyox - Chapter 01 - Final Method

---
+++ chapters/chapter01/FinalMethod.java
@@ -0,0 +1,11 @@
+package chapters.chapter01;
+
+public class FinalMethod{
+  public final void method1(){}
+  public static void main(String [] args){}
+}
+
+class Method extends FinalMethod{
+  public void method1(){}
+  public void method2(){}
+}
