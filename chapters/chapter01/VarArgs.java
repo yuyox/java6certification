ce2fd7c9cf4b38d4afc046795ff26d85e1993ccb
yuyox <ezequielrmy@yahoo.com.ar>
2016-03-26 19:30:29 -0500

    Yuyox - Chapter 01 - Variable Arguments

---
+++ chapters/chapter01/VarArgs.java
@@ -0,0 +1,11 @@
+package chapters.chapter01;
+
+public class VarArgs{
+
+  public static void main(String[] args){}
+  public VarArgs0(int... i){}
+  public VarArgs1(int... i, String string, double... d){}
+  public VarArgs2(String string, double... d){}
+  public VarArgs3(int... i, String string){}
+
+}
