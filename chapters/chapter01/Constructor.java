f24883aac98457607e5d15867462093ced46cb3b
yuyox <ezequielrmy@yahoo.com.ar>
2016-03-26 19:41:34 -0500

    Yuyox - Chapter 01 - Constructors

---
+++ chapters/chapter01/Constructor.java
@@ -0,0 +1,11 @@
+package chapters.chapter01;
+
+public class Constructor{
+
+  public static void main(String[] args){}
+  private Constructor(){}
+  private Constructor(int i){}
+  final Constructor(int i){}
+  public Constructor(int i, int I){}
+
+}
